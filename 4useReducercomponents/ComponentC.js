31b66e6dbaba12dcb32827a6e8d8beca55f647d1
Kerim Imamovic <79793398+kimamovic21@users.noreply.github.com>
2022-03-09 16:28:33 +0100

    Add files via upload

---
+++ 4useReducercomponents/ComponentC.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComponentE from './4useReducercomponents/ComponentE'
+
+function ComponentC() {
+    return (
+        <div>
+            <ComponentE/>
+        </div>  
+    )
+}
+
+export default ComponentC
---
+++ 4useReducercomponents/ComponentE.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComponentF from './4useReducercomponents/ComponentF'
+
+function ComponentE() {
+    return (
+        <div>
+            <ComponentF/>
+        </div>
+    )
+}
+
+export default ComponentE
