a889c16201dfc093aa569a8570fcc549a3301947
Kerim Imamovic <79793398+kimamovic21@users.noreply.github.com>
2022-03-09 16:29:00 +0100

    Add files via upload

---
+++ ComponentB.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComponentD from './4useReducercomponents/ComponentD'
+
+function ComponentB() {
+    return (
+        <div>
+            <ComponentD/>
+        </div>
+    )
+}
+
+export default ComponentB
